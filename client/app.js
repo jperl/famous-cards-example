4b6e0c29ee6f211eeb9cd6a5a1ee43711cd8bc5f
Jon Perl <perl.jonathan@gmail.com>
2014-05-20 16:20:04 -0700

    Lock famo.us version, now works.

--- client/app.js
+++ client/app.js
@@ -1,28 +1,24 @@
-require("famous-polyfills"); // Add polyfills
-require("famous/core/famous"); // Add the default css file
+require('famous-polyfills'); // Add polyfills
+require('famous/core/famous'); // Add the default css file
 
 // Include all the famous dependencies.
 var Engine = require('famous/core/Engine');
 var Modifier = require('famous/core/Modifier');
-var ImageSurface = require('famous/surfaces/ImageSurface');
-
 var RenderNode = require('famous/core/RenderNode');
 
 var MouseSync = require('famous/inputs/MouseSync');
 var TouchSync = require('famous/inputs/TouchSync');
 var ScrollSync = require('famous/inputs/ScrollSync');
 var GenericSync = require('famous/inputs/GenericSync');
-
 GenericSync.register({
     mouse: MouseSync,
     touch: TouchSync,
     scroll: ScrollSync
 });
 
-var CardScrollview = require('famous-cards/CardScrollview');
+var ImageSurface = require('famous/surfaces/ImageSurface');
 
-// create the main context
-var mainContext = Engine.createContext();
+var CardScrollview = require('famous-cards/CardScrollview');
 
 // Add the background famous image
 var backgroundNode = new RenderNode(new Modifier({
@@ -32,12 +28,11 @@ backgroundNode.add(new ImageSurface({
     size: [200, 200],
     content: 'famous_logo.png'
 }));
-mainContext.add(backgroundNode);
 
 // Create the card scrollview.
 var cards = new CardScrollview();
 
-// Add 10 example cards.
+//Add 10 example cards.
 for (var i = 0; i < 10; i++) {
     var card = new ExampleCard({
         _id: i
@@ -45,4 +40,7 @@ for (var i = 0; i < 10; i++) {
     cards.addCard(card);
 }
 
+// create the main context
+var mainContext = Engine.createContext();
+mainContext.add(backgroundNode);
 mainContext.add(cards);
